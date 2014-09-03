79b750a36907646618e15778399670b21fc90209
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-09-03 21:52:21 +0200

    Fix ExternalSort to properly call the bash sort.

--- src/main/java/de/mpii/spotter/ExternalSort.java
+++ src/main/java/de/mpii/spotter/ExternalSort.java
@@ -1,19 +1,19 @@
 package de.mpii.spotter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class ExternalSort {
 
-	public static int sort(String inputPath, String outputPath, char separator,
+	public static int sort(String inputPath, String outputPath, char delimiter,
 			int keyField, boolean isNumeric, boolean doShuffle) throws IOException {
 		ArrayList<String> command = new ArrayList<String>();
-		command.add("/bin/bash");
-		command.add("-c");
+		command.add("sort");
 		command.add("-t");
-		command.add("'" + String.valueOf(separator) + "'");
-		command.add("-k");
-		command.add(String.valueOf(keyField));
+		command.add(String.valueOf(delimiter));
+		command.add("-k" + String.valueOf(keyField));
 		if (isNumeric) {
 			command.add("-n");
 		}
@@ -21,13 +21,35 @@ public class ExternalSort {
 			command.add("-R");
 		}
 		command.add(inputPath);
-		command.add(">");
+		command.add("-o");
 		command.add(outputPath);
 		
-		System.out.println(String.join(" ", command));
+		//System.out.println("Executing command: " + command);
 		
 		Process pb = Runtime.getRuntime().exec(command.toArray(new String[]{}));
-		
+		try {
+			pb.waitFor();
+		} catch (InterruptedException e) {
+			return -1;
+		}
+//		StringBuffer output = new StringBuffer();
+//		BufferedReader reader = 
+//                new BufferedReader(new InputStreamReader(pb.getInputStream()));
+//
+//            String line = "";			
+//		while ((line = reader.readLine())!= null) {
+//			output.append(line + "\n");
+//		}
+//		System.out.println("output: " + output.toString());
+
 		return pb.exitValue();
+	}
+	
+	public static void main(String args[]) throws IOException {
+		int returnValue = ExternalSort.sort("/tmp/test.txt", "/tmp/sort_test.txt", '\t', 2, true, false);
+		System.out.println("Return Value = " + returnValue);
+
+		returnValue = ExternalSort.sort("/tmp/test.txt", "/tmp/random_test.txt", '\t', 1, false, false);
+		System.out.println("Return Value = " + returnValue);
 	}
 }
